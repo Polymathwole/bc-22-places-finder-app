a48f38dfbf345a78cf5b03d5ea85eaba43c987a2
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-05-16 16:04:02 +0100

    Update main

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let e = require("express");
 let app = e();
 let exphbs  = require('express-handlebars');
+let firebase = require("firebase");
+
+let config = {
+    apiKey: "AIzaSyAa7vfOxueB1yw0GmZVnvuBqovAzXt18Pk",
+    authDomain: "places-finder-app.firebaseapp.com",
+    databaseURL: "https://places-finder-app.firebaseio.com",
+    projectId: "places-finder-app",
+    storageBucket: "places-finder-app.appspot.com",
+    messagingSenderId: "26417830481"
+  };
+
+firebase.initializeApp(config);
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -17,6 +30,10 @@ app.get('/dashboard', function(req, res){
                 namee: req.query.pw
         });
 });
+
+app.get('/authenticateUser',()=>{
+        
+})
 
 app.get('/signup', function(req, res){
         res.render("signup")
