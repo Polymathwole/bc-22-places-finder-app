d427c7573eb053a70fef920738011ec5b2963507
Oluwaremi Oluwole <polymathwole@gmail.com>
2017-05-17 10:30:03 +0100

    Add email sign in/out Firebase authentication

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 let e = require("express");
 let app = e();
 let exphbs  = require('express-handlebars');
+let bp=require('body-parser');
+let firebase=require('firebase');
+
+let config = {
+    apiKey: "AIzaSyAa7vfOxueB1yw0GmZVnvuBqovAzXt18Pk",
+    authDomain: "places-finder-app.firebaseapp.com",
+    databaseURL: "https://places-finder-app.firebaseio.com",
+    projectId: "places-finder-app",
+    storageBucket: "places-finder-app.appspot.com",
+    messagingSenderId: "26417830481"
+  };
+
+firebase.initializeApp(config);
+const auth=firebase.auth();
+app.use(bp());
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -11,22 +26,51 @@ app.get('/', function(req, res){
 });
 
 
-app.get('/dashboard', function(req, res){
-        res.render("dashboard",{
-                 nam: req.query.em,
-                namee: req.query.pw
+app.post('/dashboard', function(req, res){
+        console.log(req.body);
+        auth.signInWithEmailAndPassword(req.body.em, req.body.pw).then((resp)=>{
+                if(resp.uid){
+                        let trm=req.body.em.split("@")[0];
+                        res.render("dashboard",{nam: trm, logout:req.route.path.split('d')[0]+"logout"});                        
+                }
+        }).catch((e)=>{
+                console.log(e)
+                res.redirect('/');
         });
 });
 
-app.get('/signup', function(req, res){
-        res.render("signup")
+app.get('/logout', function(req, res){
+        auth.signOut();
+        res.render("logout",{login:req.route.path.split('l')[0]});
 });
+
+app.get('/signup', function(req, res){
+        res.render("signup");
+});
+
+app.post('/authenticate',function(req,res){
+        console.log(req.body);
+        auth.createUserWithEmailAndPassword(req.body.em, req.body.pw).catch((e)=>console.log(e));       
+
+}
+);
+
+auth.onAuthStateChanged((user)=>{
+        if (user) 
+        {
+        console.log('user logged in')
+        } 
+        else 
+        {
+        console.log('user logged out')
+        }});
+
+
 
 app.use(function(req, res){
         res.status(404);
         res.render('notfound');
 });
-
 
 
 app.listen(8080, function(){
